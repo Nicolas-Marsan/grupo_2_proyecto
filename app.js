7563371cf74ae969c1c4b0fabd8b61863becbc68
Nicolas-Marsan <nicomar80@gmail.com>
2021-04-22 17:55:10 -0300

    agregando encabezado

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const path= require('path');
 
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.listen(3000, () => {
     console.log('Servidor Corriendo en el puerto 3000');
     
 })
 app.get('/', function(req , res){
-    
+    let htmlPath = path.join(__dirname,'./views/index.html' );
+    res.sendFile(htmlPath);
 });
 
 
