690dc3217b217dc47a756d3c778c3279fdac2261
rodrifernandez7 <fernandezmrodrigo7@gmail.com>
2021-05-14 16:35:08 -0300

    instalacion e implementacion de ejs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path= require('path');
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const rutasProducts = require('./routes/products.js');
