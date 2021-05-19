30e2676b57adff5232819ff06fa418d6f61399ee
rodrifernandez7 <fernandezmrodrigo7@gmail.com>
2021-05-19 10:02:06 -0300

    agregando comentarios

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path= require('path');
 
+/* Configuración de ejs */
 app.set('view engine', 'ejs');
-
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+/* Rutas */
 const rutasProducts = require('./routes/products.js');
 const rutasUsers = require ('./routes/users.js');
 const rutasHome = require ('./routes/home.js');
@@ -15,7 +16,7 @@ app.use('/product', rutasProducts);
 app.use('/users', rutasUsers);
 app.use('/crear', rutasProducts);
 
-
+/* Servidor */
 app.listen(3000, () => {
     console.log('Servidor Corriendo en el puerto 3000');
 });
