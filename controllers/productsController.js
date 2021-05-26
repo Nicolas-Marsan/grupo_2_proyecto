ddd2d14739f29e515d067039ed40d90e3973494c
FrancoSpinelli <franco_spinelli@hotmail.com>
2021-05-26 16:10:45 -0300

    vista products ejs

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const fs = require ('fs');
+
+let products = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/products.json'),{encoding:'utf-8'})); //Leer el JSON y pasarlo a objeto literal
 
 const productsController = {
     index: function(req, res){
-        res.render('products')
+        let newModels = products.filter((product)=>{return product.newmodel}); // productos nuevos
+        let favoriteProducts = products.filter((product)=>{return product.favorite}); // productos favoritos
+        res.render('products', {favoriteProducts,newModels});
     },
     productDetail: function(req , res){
         /* let htmlPath = path.join(__dirname,'../views/productDetail.html');
