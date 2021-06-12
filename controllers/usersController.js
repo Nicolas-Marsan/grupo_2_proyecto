7b7ce75eb93485a36b4fb15a1e5c578c0890d0e5
GastonMolinari <gatimolinari@hotmail.com>
2021-06-12 14:02:57 -0300

    Register sprint 5

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,44 @@
+const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const User = require('../models/Users');
+const bcryptjs = require('bcryptjs'); //*hasheare contra/
 
 const usersController = {
+    register: function (req , res){
+        /* let htmlPath = path.join(__dirname,'../views/register.html' );
+        res.sendFile(htmlPath); */
+        res.render('register');
+    },
+
+    updateR: function(req, res) {
+        
+        /* let userInDB = User.findByField('email', req.body.email);
+        if (userInDB) {
+        return res.render('register', {
+        oldData: req.body
+       })
+    } */   
+       let bodyEntero = {
+            ...req.body,
+            password: bcryptjs.hashSync(req.body.password, 10), //* hashear contraseña/
+            Image: req.file.filename, //* pedir el nombre que le dimos a la imagen /
+        }
+
+
+       User.create(bodyEntero);
+        return res.redirect('/users/login')
+    },
+    
     login: function(req , res){
         /* let htmlPath = path.join(__dirname,'../views/login.html' );
         res.sendFile(htmlPath); */
         res.render('login');
     },
-    register: function (req , res){
-        /* let htmlPath = path.join(__dirname,'../views/register.html' );
-        res.sendFile(htmlPath); */
-        res.render('register');
+    processLogin: function(req , res) {
+        
     }
+    
 };
 
 module.exports = usersController;
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,29 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const usersController = require ('../controllers/usersController.js');
+const multer = require('multer');
 
-router.get('/login', usersController.login);
+let storage = multer.diskStorage({    
+	destination: (req,file,callback) => {
+    let folder = path.join(__dirname, '../public/images/users');
+    console.log(folder)
+ callback(null, folder);
+},
+filename: (req,file,callback) =>{
+    let imageName = file.fieldname +  Date.now() + path.extname(file.originalname);
+ callback(null, imageName);
+}
+});
+
+const fileUpload = multer({ storage });
 
 router.get('/register', usersController.register);
+router.post('/register', fileUpload.single('image'), usersController.updateR);
+
+
+
+router.get('/login', usersController.login);
+router.post('/login', usersController.processLogin);
+
 
 module.exports = router;
