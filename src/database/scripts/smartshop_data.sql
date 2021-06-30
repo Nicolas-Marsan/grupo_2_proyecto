beca4a0b8a2266d8cb47bee070769cfb74aa117f
FrancoSpinelli <franco_spinelli@hotmail.com>
2021-06-30 16:33:34 -0300

    agregado de scripts y DER

---
+++ src/database/scripts/smartshop_data.sql
@@ -0,0 +1,102 @@
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (1, 'Dew', 'Kitchener', 'dkitchener0@ftc.gov', 'zN9wh4h3KN', true, false);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (2, 'Allina', 'Cheshir', 'acheshir1@ihg.com', '2kfUDp2Ts', true, false);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (3, 'Katrinka', 'Vakhrushin', 'kvakhrushin2@networkadvertising.org', 'iApuMtId0', true, false);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (4, 'Cedric', 'Hawksley', 'chawksley3@howstuffworks.com', '6H953F', true, false);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (5, 'Salim', 'Taffee', 'staffee4@bigcartel.com', 'xfaTexy3Mm', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (6, 'Shel', 'Davidowich', 'sdavidowich5@example.com', 'zriy64hEF', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (7, 'Carleton', 'Frunks', 'cfrunks6@qq.com', 'vIHvCxR1N', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (8, 'Olivia', 'Levesque', 'olevesque7@zdnet.com', 'MvnzuhzT', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (9, 'Nappy', 'Sinclaire', 'nsinclaire8@cisco.com', '0aIBZ4', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (10, 'Angelita', 'Adamowitz', 'aadamowitz9@mayoclinic.com', 'MVSSl4JoSmel', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (11, 'Jared', 'Pauly', 'jpaulya@imdb.com', 'Uxf7LQhyT2', true, false);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (12, 'Callie', 'Bricham', 'cbrichamb@oracle.com', 'iyFrCNP0c8E', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (13, 'Nerte', 'Urlin', 'nurlinc@salon.com', 'SCcMA94dOZ', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (14, 'Tull', 'Jukubczak', 'tjukubczakd@sakura.ne.jp', 'O60io0PP0s', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (15, 'Rourke', 'Shavel', 'rshavele@omniture.com', 'LlM1jKSo', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (16, 'Ozzie', 'Muehle', 'omuehlef@shop-pro.jp', '3YgF58UkSJP', true, false);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (17, 'Hamilton', 'Maron', 'hmarong@deliciousdays.com', 'jAqJ1YZoqNP', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (18, 'Flint', 'Melliard', 'fmelliardh@mediafire.com', 'AiG7bw', true, false);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (19, 'Monah', 'Ashfold', 'mashfoldi@economist.com', '9nZERC', true, true);
+insert into usuarios (id, nombre, apellido, mail, contraseña, tyc, newletter) values (20, 'Mahmud', 'Fallows', 'mfallowsj@pagesperso-orange.fr', 'lU80QKUr', true, true);
+
+
+
+INSERT INTO `smartshop_db`.`marcas` (`nombre`) VALUES ('Apple');
+INSERT INTO `smartshop_db`.`marcas` (`nombre`) VALUES ('Samsung');
+INSERT INTO `smartshop_db`.`marcas` (`nombre`) VALUES ('Motorola');
+INSERT INTO `smartshop_db`.`marcas` (`nombre`) VALUES ('Xiaomi');
+
+
+
+INSERT INTO `smartshop_db`.`colores` (`nombre`) VALUES ('Negro');
+INSERT INTO `smartshop_db`.`colores` (`nombre`) VALUES ('Azul');
+INSERT INTO `smartshop_db`.`colores` (`nombre`) VALUES ('Dorado');
+INSERT INTO `smartshop_db`.`colores` (`nombre`) VALUES ('Blanco');
+INSERT INTO `smartshop_db`.`colores` (`nombre`) VALUES ('Rojo');
+INSERT INTO `smartshop_db`.`colores` (`nombre`) VALUES ('Rosa');
+INSERT INTO `smartshop_db`.`colores` (`nombre`) VALUES ('Celeste');
+
+
+
+INSERT INTO `smartshop_db`.`rams` (`valor`) VALUES ('3');
+INSERT INTO `smartshop_db`.`rams` (`valor`) VALUES ('4');
+INSERT INTO `smartshop_db`.`rams` (`valor`) VALUES ('6');
+
+
+
+INSERT INTO `smartshop_db`.`categorias` (`nombre`) VALUES ('Nuevo');
+INSERT INTO `smartshop_db`.`categorias` (`nombre`) VALUES ('Usado');
+INSERT INTO `smartshop_db`.`categorias` (`nombre`) VALUES ('Favorito');
+
+
+
+INSERT INTO `smartshop_db`.`pantallas` (`tamaño`, `resolucion`) VALUES ('4.7', '1334 x 750');
+INSERT INTO `smartshop_db`.`pantallas` (`tamaño`, `resolucion`) VALUES ('5.4', '2340 x 1080');
+INSERT INTO `smartshop_db`.`pantallas` (`tamaño`, `resolucion`) VALUES ('6,1', '2532 x 1170');
+INSERT INTO `smartshop_db`.`pantallas` (`tamaño`, `resolucion`) VALUES ('6.1', '1792 x 828');
+
+
+
+INSERT INTO `smartshop_db`.`sistemas_operativos` (`nombre`) VALUES ('iOS');
+INSERT INTO `smartshop_db`.`sistemas_operativos` (`nombre`) VALUES ('Android');
+
+
+
+INSERT INTO `smartshop_db`.`memorias` (`valor`) VALUES ('64');
+INSERT INTO `smartshop_db`.`memorias` (`valor`) VALUES ('128');
+INSERT INTO `smartshop_db`.`memorias` (`valor`) VALUES ('256');
+INSERT INTO `smartshop_db`.`memorias` (`valor`) VALUES ('512');
+INSERT INTO `smartshop_db`.`memorias` (`valor`) VALUES ('1024');
+
+
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('Apple A14');
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('Apple A13');
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('Apple A12');
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('Exynos 850 Ocho');
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('Octa Core');
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('Qualcomm Snapdragon');
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('Exynos 990');
+INSERT INTO `smartshop_db`.`procesadores` (`nombre`) VALUES ('MediaTek Helio G85');
+
+
+
+INSERT INTO `smartshop_db`.`usuarios_ordenes` (`usuario_id`, `orden_id`) VALUES ('1', '1');
+INSERT INTO `smartshop_db`.`usuarios_ordenes` (`usuario_id`, `orden_id`) VALUES ('1', '2');
+INSERT INTO `smartshop_db`.`usuarios_ordenes` (`usuario_id`, `orden_id`) VALUES ('1', '3');
+INSERT INTO `smartshop_db`.`usuarios_ordenes` (`usuario_id`, `orden_id`) VALUES ('2', '4');
+INSERT INTO `smartshop_db`.`usuarios_ordenes` (`usuario_id`, `orden_id`) VALUES ('3', '5');
+INSERT INTO `smartshop_db`.`usuarios_ordenes` (`usuario_id`, `orden_id`) VALUES ('3', '6');
+INSERT INTO `smartshop_db`.`usuarios_ordenes` (`usuario_id`, `orden_id`) VALUES ('4', '7');
+
+
+
+INSERT INTO `smartshop_db`.`productos` (`modelo`, `marca_id`, `categoria_id`, `sistema_operativo_id`, `ram_id`, `memoria_id`, `pantalla_id`, `procesador_id`, `color_id`, `stock`, `precio_unitario`) VALUES ('iPhone 12', '1', '1', '1', '3', '2', '3', '1', '5', '3', '25000');
+INSERT INTO `smartshop_db`.`productos` (`modelo`, `marca_id`, `categoria_id`, `sistema_operativo_id`, `ram_id`, `memoria_id`, `pantalla_id`, `procesador_id`, `color_id`, `stock`, `precio_unitario`) VALUES ('Moto G9 Plus', '2', '2', '2', '1', '1', '1', '2', '2', '2', '20000');
+INSERT INTO `smartshop_db`.`productos` (`modelo`, `marca_id`, `categoria_id`, `sistema_operativo_id`, `ram_id`, `memoria_id`, `pantalla_id`, `procesador_id`, `color_id`, `stock`, `precio_unitario`) VALUES ('Samsung Galaxy A51', '3', '3', '2', '2', '2', '2', '2', '3', '5', '15000');
+
+
+
+INSERT INTO `smartshop_db`.`ordenes_detalles` (`producto_id`, `cantidad`, `total`, `envio`) VALUES ('1', '2', '50000', '1');
+INSERT INTO `smartshop_db`.`ordenes_detalles` (`producto_id`, `cantidad`, `total`, `envio`) VALUES ('3', '1', '15000', '0');
+INSERT INTO `smartshop_db`.`ordenes_detalles` (`producto_id`, `cantidad`, `total`, `envio`) VALUES ('3', '2', '30000', '0');
+INSERT INTO `smartshop_db`.`ordenes_detalles` (`producto_id`, `cantidad`, `total`, `envio`) VALUES ('2', '1', '2000', '1');
---
+++ src/database/scripts/smartshop_structure.sql
@@ -0,0 +1,173 @@
+    -- MySQL Workbench Forward Engineering
+
+    SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+    SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+    SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+    
+    -- -----------------------------------------------------
+    -- Schema smartshop_db
+    -- -----------------------------------------------------
+    
+    -- -----------------------------------------------------
+    -- Schema smartshop_db
+    -- -----------------------------------------------------
+    CREATE SCHEMA IF NOT EXISTS `smartshop_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
+    USE `smartshop_db` ;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`usuarios`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`usuarios` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `nombre` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      `apellido` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      `mail` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      `contraseña` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      `tyc` TINYINT(1) NOT NULL,
+      `newletter` TINYINT(1) NULL DEFAULT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`marcas`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`marcas` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `nombre` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`categorias`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`categorias` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `nombre` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`sistemas_operativos`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`sistemas_operativos` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `nombre` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`rams`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`rams` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `valor` INT NOT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`memorias`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`memorias` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `valor` INT NOT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`pantallas`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`pantallas` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `tamaño` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      `resolucion` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NULL DEFAULT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`procesadores`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`procesadores` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `nombre` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`colores`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`colores` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `nombre` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`productos`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`productos` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `modelo` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NOT NULL,
+      `marca_id` INT NOT NULL,
+      `categoria_id` INT NOT NULL,
+      `sistema_operativo_id` INT NOT NULL,
+      `ram_id` INT NOT NULL,
+      `memoria_id` INT NOT NULL,
+      `pantalla_id` INT NOT NULL,
+      `procesador_id` INT NOT NULL,
+      `color_id` INT NOT NULL,
+      `imagen` VARCHAR(255) COLLATE utf8mb4_unicode_ci  NULL,
+      `stock` INT NOT NULL DEFAULT 3,
+	  `precio_unitario` INT NOT NULL,
+      PRIMARY KEY (`id`),
+      INDEX `marca_id_idx` (`marca_id` ASC),
+      INDEX `categoria_id_idx` (`categoria_id` ASC),
+      INDEX `sistema_id_idx` (`sistema_operativo_id` ASC),
+      INDEX `ram_id_idx` (`ram_id` ASC),
+      INDEX `memoria_id_idx` (`memoria_id` ASC),
+      INDEX `pantalla_id_idx` (`pantalla_id` ASC),
+      INDEX `procesador_id_idx` (`procesador_id` ASC),
+      INDEX `color_id_idx` (`color_id` ASC))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`ordenes_detalles`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`ordenes_detalles` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `producto_id` INT NOT NULL,
+      `cantidad` INT NOT NULL DEFAULT 1,
+      `total` INT NOT NULL,
+      `envio` TINYINT(1) NULL DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      INDEX `producto_id_idx` (`producto_id` ASC))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    -- -----------------------------------------------------
+    -- Table `smartshop_db`.`usuarios_ordenes`
+    -- -----------------------------------------------------
+    CREATE TABLE IF NOT EXISTS `smartshop_db`.`usuarios_ordenes` (
+      `id` INT NOT NULL AUTO_INCREMENT,
+      `usuario_id` INT NOT NULL,
+      `orden_id` INT NOT NULL,
+      PRIMARY KEY (`id`),
+      INDEX `usuario_id_idx` (`usuario_id` ASC),
+      INDEX `orden_id_idx` (`orden_id` ASC))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    
+    
+    SET SQL_MODE=@OLD_SQL_MODE;
+    SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+    SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+    
