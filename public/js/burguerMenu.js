696e63d6da1b5695072666a9a0ea4bdea9444e6c
Nicolas-Marsan <nicomar80@gmail.com>
2021-09-07 18:04:58 -0300

    mejorando buerguer menu

--- public/js/burguerMenu.js
+++ public/js/burguerMenu.js
@@ -1,26 +1,50 @@
 window.onload = function() {
-
+console.log(window.innerWidth);
 let menu=document.querySelector('.hamburguesa');
 let hambu=document.querySelector('.menuOculto');
-let cabeza=document.querySelector('header');
+let cabeza=document.querySelector('main');
+let tres=document.querySelector('.burger');
+
+/*tres.addEventListener('click',function(){
+
+    alert(tres.className);
+})*/
+
+//alert('hizo click');
 menu.addEventListener('click',function(){
     //alert('hizo click');
     //menu.style.color= 'red';
+    
     if(hambu.style.display == 'none'){
     hambu.style.display = 'inline';
+    tres.classList.remove("burger");
+    tres.classList.add("fa-times");    
+
     }else{
         hambu.style.display = 'none'; 
+        tres.classList.remove("fa-times");
+        tres.classList.add("burger");
     }
+  
+
 })
 
+cabeza.addEventListener('click', function(){
 
-menu.addEventListener('mouseOut',function(){
-    //alert('hizo click');
-    //menu.style.color= 'red';
-    
-        hambu.style.display = 'none'; 
-    
+    hambu.style.display = 'none'; 
+    tres.classList.remove("fa-times");
+    tres.classList.add("burger");
+
 })
+
+window.addEventListener('resize', function(){
+
+    hambu.style.display = 'none'; 
+    tres.classList.remove("fa-times");
+    tres.classList.add("burger");
+
+})
+
 
 
 }
